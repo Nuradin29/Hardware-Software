f9ea5bca2475594083c2250e4b43bf7d076d806c
yusufgoksu <goksuy@mef.edu.tr>
2025-05-19 16:52:42 +0100

    updated

---
+++ src/Acceptor.kt
@@ -0,0 +1,10 @@
+object Acceptor {
+    fun isCoinInserted() = HAL.isBit(Masks.I6)
+    fun coinID() = if (HAL.isBit(Masks.I5)) 1 else 0
+    fun acceptCoin() {
+        HAL.setBits(Masks.O6)
+    }
+    fun stopAccepting() {
+        HAL.clrBits(Masks.O6)
+    }
+}
---
+++ src/Masks.kt
@@ -0,0 +1,20 @@
+object Masks {
+    // Inputs
+    const val I0 = 0x01
+    const val I1 = 0x02
+    const val I2 = 0x04
+    const val I3 = 0x08
+    const val I4 = 0x10
+    const val I5 = 0x20
+    const val I6 = 0x40
+    const val I7 = 0x80
+
+    // Outputs
+    const val O0 = 0x01
+    const val O1 = 0x02
+    const val O2 = 0x04
+    const val O3 = 0x08
+    const val O4 = 0x10
+    const val O6 = 0x40
+    const val O7 = 0x80
+}
